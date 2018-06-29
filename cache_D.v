77d041c022648400d855c9a806d66cb7e7764f4f
brunodamacena <damacena.bruno@gmail.com>
2018-06-29 11:56:51 -0300

    blocks

---
+++ cache_D.v
@@ -0,0 +1,45 @@
+module cache_D(
+	clock,
+	write,
+	state,
+	owner,
+	address,
+	data_in,
+	current_state,
+	current_owner,
+	current_address,
+	current_data
+);
+
+	input clock;
+	input write;
+	input [1:0] state;
+	input [1:0] owner;
+	input [2:0] address;
+	input [3:0] data_in;
+	
+	output reg [1:0] current_state;
+	output reg [1:0] current_owner;
+	output reg [2:0] current_address;
+	output reg [3:0] current_data;
+	
+	initial begin
+		current_state = 2'b00; //D Invalid
+		current_owner = 2'b00;
+		current_address = 3'b000;
+		current_data = 4'b0000;
+	end
+	
+	always @(posedge clock)
+	begin		
+		if(write)
+		begin
+			current_state = state;
+			current_owner = owner;
+			current_address = address;
+			current_data = data_in;
+		end
+	end
+	
+
+endmodule
---
+++ memory.v
@@ -0,0 +1,38 @@
+module memory(
+	clock,
+	write,
+	address,
+	data_in,
+	data_out
+);
+
+	input clock;
+	input write;
+	input [2:0] address;
+	input [3:0] data_in;
+	
+	output reg [3:0] data_out;
+	
+	reg [3:0] memory_data[7:0];
+	
+	initial begin
+		memory_data[0] = 4'b0000;
+		memory_data[1] = 4'b0000;
+		memory_data[2] = 4'b0000;
+		memory_data[3] = 4'b0000;
+		memory_data[4] = 4'b0000;
+		memory_data[5] = 4'b0000;
+		memory_data[6] = 4'b0000;
+		memory_data[7] = 4'b0000;
+	end
+	
+	always @(posedge clock)
+	begin		
+		if(write)
+		begin
+			memory_data[address] = data_in;
+		end
+		data_out = memory_data[address];
+	end
+
+endmodule
